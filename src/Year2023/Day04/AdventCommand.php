b31f4c609e5fa5b0fef27d436b34c6cf962d58aa
Nekwo <just.nico@yahoo.de>
2023-12-04 09:21:59 +0100

    feat: complete day4

--- src/Year2023/Day04/AdventCommand.php
+++ src/Year2023/Day04/AdventCommand.php
@@ -17,11 +17,7 @@ use Symfony\Component\Filesystem\Filesystem;
 )]
 class AdventCommand extends Command
 {
-    private array $lottery = [
-        'winners' => [],
-        'pool' => [],
-    ];
-
+    private array $lottery = [];
     private array $winners = [];
 
     protected function configure(): void
@@ -42,17 +38,21 @@ class AdventCommand extends Command
         $filesystem = new Filesystem();
         if ($filesystem->exists($filepath)) {
             $scratchCardPoints = 0;
+            $collectedScratchCards = 0;
 
             foreach ($this->readLinesFromFile($filepath) as $key => $line) {
                 $output->write(sprintf('<comment>%s</comment>', $line));
 
-                $this->populateLottery($line);
+                $this->populateLottery($line, $key);
                 $this->getMatchingWinners($key);
             }
             $output->writeln('');
 
             $scratchCardPoints = array_sum(array_map(static fn ($winner) => pow(2, count($winner)) / 2, $this->winners));
-            $output->writeln(sprintf('<info>Solution 1: %s</info>', $scratchCardPoints));
+            $output->writeln(sprintf('<info>Solution 1: %d</info>', $scratchCardPoints));
+
+            $collectedScratchCards = $this->collectScratchCards();
+            $output->writeln(sprintf('<info>Solution 2: %d</info>', $collectedScratchCards));
 
             return Command::SUCCESS;
         }
@@ -61,21 +61,41 @@ class AdventCommand extends Command
         return Command::FAILURE;
     }
 
-    private function populateLottery(string $line): void
+    private function populateLottery(string $line, int $key): void
     {
-        $this->lottery['winners'][] = array_filter(explode(' ', trim(explode(':', explode('|', $line)[0])[1])), static fn ($val) => $val !== '');
-        $this->lottery['pool'][] = array_filter(explode(' ', trim(explode('|', $line)[1])), static fn ($val) => $val !== '');
+        $this->lottery[$key]['winners'] = array_filter(explode(' ', trim(explode(':', explode('|', $line)[0])[1])), static fn ($val) => $val !== '');
+        $this->lottery[$key]['pool'] = array_filter(explode(' ', trim(explode('|', $line)[1])), static fn ($val) => $val !== '');
+        $this->lottery[$key]['count'] = 1;
     }
 
     private function getMatchingWinners(int $key): array
     {
-        array_walk_recursive($this->lottery['winners'][$key], function ($number) use ($key) {
-            if (in_array($number, $this->lottery['pool'][$key])) {
+        array_walk_recursive($this->lottery[$key]['winners'], function ($number) use ($key) {
+            if (in_array($number, $this->lottery[$key]['pool'])) {
                 $this->winners[$key][] = $number;
             }
         });
 
         return $this->winners;
+    }
+
+    private function collectScratchCards(): int
+    {
+        foreach ($this->lottery as $key => &$cards) {
+            foreach (range(1, $cards['count']) as $rounds) {
+                $i = 1;
+
+                foreach ($cards['winners'] as $number) {
+                    if (in_array($number, $cards['pool'])) {
+                        if (isset($this->lottery[$key + $i])) {
+                            $this->lottery[$key + $i++]['count'] += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return array_sum(array_column($this->lottery, 'count'));
     }
 
     private function readLinesFromFile(string $filepath): Generator
